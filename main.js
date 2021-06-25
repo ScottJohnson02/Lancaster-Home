dfc9ac6e697d832238e1a6615baba229e708443d
ScottJohnson02 <ScottyJohnson002@gmail.com>
2021-06-25 19:10:38 -0400

    Added more CSS to hero banner and Nav
    Used javascript to add color to nav when scrolled and added button styling

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("test");
+window.onscroll = function() {
+  var element = document.getElementById("nav-id");
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0) {
+    element.classList.add('scrolled')
+  } else {
+    element.classList.remove('scrolled')
+  }
+}
