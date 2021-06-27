6ada9402896293437190096542b2208b72e97266
ScottJohnson02 <ScottyJohnson002@gmail.com>
2021-06-27 17:24:40 -0400

    add portfolio overview page
    edited nav links, renamed images, create category system.

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ window.onscroll = function() {
     element.classList.remove('scrolled')
   }
 }
+
+
+//create a function to make certain classes vissable or invissible when button is clicked
+
+//classes: all kitchen bathrooms decks
+
+function hideProjects(button) {
+  var all_projects = document.getElementsByClassName('all');
+  var all_menu_items = document.getElementsByClassName('project-filters');
+  var menu_item = document.getElementById(button);
+  var projects = document.getElementsByClassName(button);
+  for (i = 0; i < all_projects.length; i++) {
+    all_projects[i].style.display = 'none';
+  }
+  for (i = 0; i < projects.length; i++) {
+    projects[i].style.display = 'block';
+  }
+  for (i = 0; i < all_menu_items.length; i++) {
+    all_menu_items[i].style.color = '#55565b';
+  }
+
+  menu_item.style.color = 'black';
+}
